547fba08d3f18651d90b094e2810d7b43b07b284
Matheus <mat22.9.f.r@hotmail.com>
2021-10-27 04:03:13 -0300

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,12 @@
 // array of names of the elements to remove
 var namesTW = [
-    "live",
     "twitch-block",
     "twitch-embed",
     "twitch-chat-embed",
     "twitch-live"
 ]; 
 
-// create a functionto remove dom element "twitch" from the page
+// Loop to remove dom element "twitch" from the page
 for (let i = 0; i < namesTW.length; i++) {
     if (document.getElementById(namesTW[i]) != null) {
         var element = document.getElementById(namesTW[i]);
